88c65f42de9512d611e758eca76336e9f0acbb7b
Pedro Miguel <pedro.miguel@live.co.uk>
2018-08-21 19:52:33 +0100

    Render create artist form

--- resume/urls.py
+++ resume/urls.py
@@ -8,4 +8,5 @@ app_name = 'resume'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='artists'),
     path('artist/<uuid:artist_id>/', views.DetailView.as_view(), name='artist_detail'),
+    path('artist/create/', views.ArtistCreate.as_view(), name='artist_create'),
 ]
