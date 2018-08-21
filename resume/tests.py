9a937f6dcceb7e579aa65b092a6096ba06f152c6
Pedro Miguel <pedro.miguel@live.co.uk>
2018-08-21 19:46:24 +0100

    Test detail view returns the right object

--- resume/tests.py
+++ resume/tests.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 # app imports
 from .models import Artist
-from .views import IndexView
+from .views import IndexView, DetailView
 
 
 class TestViews(TestCase):
@@ -28,4 +28,23 @@ class TestViews(TestCase):
         response = IndexView.as_view()(request)
 
         # self.assertEqual(response.context_data['artist_list'], artist_qs)
+        self.assertEqual(response.status_code, 200)
+
+    @mock.patch('resume.views.Artist.objects')
+    def test_detail_view_return_artist(self, mock_object):
+        artist = Artist(
+            id='95e6f821-aaf6-44d5-a0fc-658cd950eed5',
+            first_name='Pedro',
+            last_name='Curado',
+            email='pedro@mail.com'
+        )
+        mock_object.get.return_value = artist
+
+        kwargs = {'artist_id': artist.id}
+        url = reverse('resume:artist_detail', kwargs=kwargs)
+        factory = RequestFactory()
+        request = factory.get(url)
+        response = DetailView.as_view()(request, **kwargs)
+
+        # self.assertEqual(response.context_data['artist'], artist)
         self.assertEqual(response.status_code, 200)
