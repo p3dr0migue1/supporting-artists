d5a36b5a9e2c0a379d969b64a26193a0d9028cd1
Pedro Miguel <pedro.miguel@live.co.uk>
2018-08-21 18:17:40 +0100

    Add "Skill" model

--- resume/models.py
+++ resume/models.py
@@ -35,3 +35,11 @@ class Language(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    artist = models.ForeignKey('Artist', blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
