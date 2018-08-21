a0af8776e3e13bc477c33d7530621a5834e24d78
Pedro Miguel <pedro.miguel@live.co.uk>
2018-08-21 18:17:46 +0100

    Add "Skill" model to admin

--- resume/admin.py
+++ resume/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import Artist, Language
+from .models import Artist, Language, Skill
 
 
 class LanguageAdmin(admin.ModelAdmin):
     readonly_fields = ('name',)
 
-    def get_read_only_fields(self, request, obj=None):
+    def get_readonly_fields(self, request, obj=None):
         if obj:
             return ['name']
         return []
@@ -17,10 +17,15 @@ class LanguageInline(admin.TabularInline):
     extra = 0
 
 
+class SkillInline(admin.TabularInline):
+    model = Skill
+    extra = 0
+
+
 class ArtistAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_created'
     readonly_fields = ('email',)
-    inlines = (LanguageInline,)
+    inlines = (LanguageInline, SkillInline)
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
@@ -30,3 +35,4 @@ class ArtistAdmin(admin.ModelAdmin):
 
 admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Language, LanguageAdmin)
+admin.site.register(Skill)
