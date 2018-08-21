b0133b915272aa0dfab5cdf0c03150bfefc55ce8
Pedro Miguel <pedro.miguel@live.co.uk>
2018-08-21 18:15:34 +0100

    Update admin with "Language" model

--- resume/admin.py
+++ resume/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 
-from .models import Artist
+from .models import Artist, Language
 
 
-admin.site.register(Artist)
+class LanguageAdmin(admin.ModelAdmin):
+    readonly_fields = ('name',)
+
+    def get_read_only_fields(self, request, obj=None):
+        if obj:
+            return ['name']
+        return []
+
+
+class LanguageInline(admin.TabularInline):
+    model = Language.artist.through
+    extra = 0
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date_created'
+    readonly_fields = ('email',)
+    inlines = (LanguageInline,)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['email']
+        return []
+
+
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Language, LanguageAdmin)
